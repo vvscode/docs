2acca40d69ae7762767ab7a16dc020a330f10e81
David Marquis <david.marquis@sinch.com>
2019-08-02 14:56:46 -0400

    Support Markdown standardization of `html`, `image`, `callout` widgets

--- replace.js
+++ replace.js
@@ -7,29 +7,80 @@
 const replace = require('replace-in-file');
 const glob = require("glob");
 
-const findSingleCodeBlock = /\[block:code]\n((.|[\r\n])*?)\n\[\/block]/;
-const findAllCodeBlocks = new RegExp(findSingleCodeBlock, 'gm');
+function codeBlockToMarkdown(json) {
+    var markdown = '';
+    for (const block of json.codes) {
+        if (block.name) {
+            markdown += `\n**${block.name}**\n`;
+        }
+        markdown += '```' + block.language + '\n';
+        markdown += block.code + '\n';
+        markdown += '```\n\n';
+    }
+    return markdown;
+}
+
+function calloutToMarkdown(json, match) {
+    // body must be pre-processed
+    const body = json.body
+        .replace(/\n/g, '\n> ');
+
+    var type;
+    switch (json.type) {
+        case 'warning': type = 'WARNING: '; break;
+        default:
+            type = '';
+    }
+
+    return `
+> **${type}${json.title}**    
+>
+> ${body}
+`;
+}
+
+function htmlToMarkdown(json) {
+    return json.html + '\n';
+}
+
+function imageToMarkdown(json) {
+    var markdown = '';
+    for (const details of json.images) {
+        markdown += `![${details.image[1]}](${details.image[0]})`;
+        if (details.caption) {
+            markdown += `\n${details.caption}`;
+        }
+        markdown += `\n`;
+    }
+    return markdown;
+}
+
+function replaceBlocks(file, blockType, conversionFunction) {
+    const blockRegex = new RegExp(`\\[block:${blockType}\\]\\n((.|[\\r\\n])*?)\\n\\[/block\\]`);
+    // when finding all blocks in a file, the flags must be set to 'global' and 'multiline'.
+    const findAllBlocks = new RegExp(blockRegex, 'gm');
+
+    replace({
+        files: file,
+        from: findAllBlocks,
+        to: (match) => {
+            // extract json from block
+            const result = match.match(blockRegex);
+            const json = JSON.parse(result[1]);
+
+            // console.log(conversionFunction(json));
+            // return match;
+
+            return conversionFunction(json);
+        },
+    });
+}
 
 glob('docs/**/*.md', {}, (er, files) => {
     for (const file of files) {
-        replace({
-            files: file,
-            from: findAllCodeBlocks,
-            to: (match) => {
-                const result = match.match(findSingleCodeBlock);
-                const details = JSON.parse(result[1]);
-
-                var replacement = '';
-                for (const block of details.codes) {
-                    if (block.name) {
-                        replacement += `\n**${block.name}**\n`;
-                    }
-                    replacement += '```' + block.language + '\n';
-                    replacement += block.code + '\n';
-                    replacement += '```\n\n';
-                }
-                return replacement;
-            },
-        });
+        replaceBlocks(file, 'code', codeBlockToMarkdown);
+        // replaceBlocks(file, 'callout', calloutToMarkdown);
+        // replaceBlocks(file, 'html', htmlToMarkdown);
+        // replaceBlocks(file, 'image', imageToMarkdown);
     }
 });
